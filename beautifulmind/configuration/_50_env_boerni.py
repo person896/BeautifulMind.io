b3b9a768581afa50623b9659fd8598314298b7c8
person896 <39915431+person896@users.noreply.github.com>
2018-06-14 14:39:15 -0400

    Add files via upload

---
+++ beautifulmind/configuration/_50_env_boerni.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+
+DATABASES = {
+    'default': {
+        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
+        'NAME'     : 'beautifulmind',
+        'USER'     : 'boerni',
+        'PASSWORD ': '',
+        'HOST'     : '127.0.0.1',
+        'PORT'     : '',
+    }
+}
+
+MEDIA_URL = 'http://192.168.100.20:8000/assets/'
+
+#if not settings.DEBUG:
+#    STATIC_URL = 'http://192.168.100.200:8000/assets/static/'
+
+COMPRESS_ENABLED = False
+DEBUG = True
+
+MINDMAPTORNADO_BIND_PORT = 1234
+MINDMAPTORNADO_SERVER = 'http://192.168.100.20:%d/ws' % (MINDMAPTORNADO_BIND_PORT)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
