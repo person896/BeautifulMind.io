c068587539ab8a74e3e1463c66a1a502eb341b28
person896 <39915431+person896@users.noreply.github.com>
2018-06-14 14:46:51 -0400

    Add files via upload

---
+++ beautifulmind/environment.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+NAME = 'microhealth'
+IS_FOR_DEVELOPMENT = True
+SECRET_KEY = 'mh1234$$'
+ADMINS = (
+	('Ian Tucker', 'ian.tucker@microhealthllc.com'),
+)
---
+++ beautifulmind/start.sh
@@ -0,0 +1,1 @@
+./manage.py runserver_plus 0.0.0.0:8000 && ./manage.py mindmaptornado_run
