3faf2113daa7b659234a18eb468266245cb5e16a
person896 <39915431+person896@users.noreply.github.com>
2018-06-14 14:41:18 -0400

    Create _50_env_microhealth.py

---
+++ beautifulmind/configuration/_50_env_microhealth.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+
+DATABASES = {
+    'default': {
+        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
+        'NAME'     : 'beautifulmind',
+        'USER'     : 'postgres',
+        'PASSWORD ': 'postgres',
+        'HOST'     : '127.0.0.1',
+        'PORT'     : '6432',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'beautifulmind-django-cache-default'
+    }
+}
+
+MEDIA_URL = 'http://beautifulmind.io/assets/'
+
+if not settings.DEBUG:
+    STATIC_URL = '%sstatic/' % (MEDIA_URL)
+
+COMPRESS_ENABLED = True
+
+MINDMAPTORNADO_BIND_PORT = 8001
+MINDMAPTORNADO_SERVER = 'http://beautifulmind.io/ws/'
+
+ADMINS = (
+    ('Bernhard', 'boerni@beautifulmind.io'),
+)
