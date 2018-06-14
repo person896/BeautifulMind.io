a229a92d52d7303aa1c5db3ad8114675de352a73
person896 <39915431+person896@users.noreply.github.com>
2018-06-14 14:38:34 -0400

    Update and rename _50_env_boerni.py to _50_env_microhealth.py

--- beautifulmind/configuration/_50_env_microhealth.py
+++ beautifulmind/configuration/_50_env_microhealth.py
@@ -6,14 +6,14 @@ DATABASES = {
     'default': {
         'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
         'NAME'     : 'beautifulmind',
-        'USER'     : 'boerni',
+        'USER'     : 'admin',
         'PASSWORD ': '',
         'HOST'     : '127.0.0.1',
         'PORT'     : '',
     }
 }
 
-MEDIA_URL = 'http://192.168.100.20:8000/assets/'
+MEDIA_URL = 'http://192.168.1.176:8000'
 
 #if not settings.DEBUG:
 #    STATIC_URL = 'http://192.168.100.200:8000/assets/static/'
